7d535e9ed07e6238feb89e75e120a3c29574f529
Matt Leininger <leiningem001@hawaii.rr.com>
2016-02-23 13:42:39 -1000

    count lines of text

--- analyzer.rb
+++ analyzer.rb
@@ -1,3 +1,9 @@
 # Text analyzer exercise from Beginning Ruby Chapter 4
 
-File.open("text.txt").each { |line| puts line }
+# variable for tracking lines of text
+line_count = 0
+
+# open the text file and count lines of text
+File.open("text.txt").each { line_count += 1 }
+
+puts line_count
