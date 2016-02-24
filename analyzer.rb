e7826660cb78eb4bedecc52dbc5caebdae2e3afa
Matt Leininger <leiningem001@hawaii.rr.com>
2016-02-24 11:15:03 -1000

    basic functionality complete

--- analyzer.rb
+++ analyzer.rb
@@ -1,9 +1,25 @@
 # Text analyzer exercise from Beginning Ruby Chapter 4
 
-# variable for tracking lines of text
-line_count = 0
+# Use File method readlines to store text lines in array
+lines = File.readlines("text.txt")
 
-# open the text file and count lines of text
-File.open("text.txt").each { line_count += 1 }
+# Store number of lines in variable
+line_count = lines.size
 
-puts line_count
+# join text lines array into a string
+text = lines.join
+characters = text
+non_white_chars = text.scan(/\S/)
+words = text.split
+sentences = text.split(/[\.\?\!]/)
+paragraphs = text.split(/\n\n/)
+
+puts "line count: #{line_count}"
+puts "all characters: #{characters.length}"
+puts "characters excluding spaces: #{non_white_chars.length}"
+puts "words: #{words.length}"
+puts "sentences: #{sentences.length}"
+puts "paragraphs: #{paragraphs.length}"
+
+puts "average words per sentence: #{words.length / sentences.length}"
+puts "average sentences per paragraph: #{sentences.length / paragraphs.length}"
