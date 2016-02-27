f7d65b547d925aa64663b8f6931b624bc5d7012c
pilauPro <leiningem001@hawaii.rr.com>
2016-02-27 11:25:33 -1000

    converting to class based functions

--- analyzer.rb
+++ analyzer.rb
@@ -1,25 +1,40 @@
 # Text analyzer exercise from Beginning Ruby Chapter 4
+class TextFile
+	def initialize(file)
+		@file = File.readlines(file)
+	end
 
+	def line_count
+		@file.size
+	end
+
+	def char_count
+		@file.join.size
+	end
+
+end
+
+puts "specify file:"
 # Use File method readlines to store text lines in array
-lines = File.readlines("text.txt")
+current = TextFile.new(gets.chomp)
 
 # Store number of lines in variable
-line_count = lines.size
+# line_count = lines.size
 
 # join text lines array into a string
-text = lines.join
-characters = text
-non_white_chars = text.scan(/\S/)
-words = text.split
-sentences = text.split(/[\.\?\!]/)
-paragraphs = text.split(/\n\n/)
+# text = lines.join
+# characters = text
+# non_white_chars = text.scan(/\S/)
+# words = text.split
+# sentences = text.split(/[\.\?\!]/)
+# paragraphs = text.split(/\n\n/)
 
-puts "line count: #{line_count}"
-puts "all characters: #{characters.length}"
-puts "characters excluding spaces: #{non_white_chars.length}"
-puts "words: #{words.length}"
-puts "sentences: #{sentences.length}"
-puts "paragraphs: #{paragraphs.length}"
+puts "line count: #{current.line_count}"
+puts "all characters: #{current.char_count}"
+# puts "characters excluding spaces: #{non_white_chars.length}"
+# puts "words: #{words.length}"
+# puts "sentences: #{sentences.length}"
+# puts "paragraphs: #{paragraphs.length}"
 
-puts "average words per sentence: #{words.length / sentences.length}"
-puts "average sentences per paragraph: #{sentences.length / paragraphs.length}"
+# puts "average words per sentence: #{words.length / sentences.length}"
+# puts "average sentences per paragraph: #{sentences.length / paragraphs.length}"
