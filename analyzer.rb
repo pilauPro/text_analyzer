d090ab2e43c31a8e588ab2d6cc99f65ffc1e376e
pilauPro <leiningem001@hawaii.rr.com>
2016-03-02 18:25:29 -1000

    change over to class based

--- analyzer.rb
+++ analyzer.rb
@@ -2,6 +2,7 @@
 class TextFile
 	def initialize(file)
 		@file = File.readlines(file)
+		@text = join_file
 	end
 
 	def line_count
@@ -13,36 +14,43 @@ class TextFile
 	end
 
 	def char_count
-		join_file.size
+		@text.size
 	end
 
 	def real_char_count
-		join_file.scan(/\S/).size
+		@text.scan(/\S/).size
+	end
+
+	def word_count
+		@text.split.size
+	end
+
+	def sentence_count
+		@text.split(/[\.\?\!]/).size
+	end
+
+	def paragraph_count
+		@text.split(/\n\n/).size
+	end
+
+	def avg_words
+		(word_count / sentence_count.to_f).round(2)
+	end
+
+	def avg_sentences
+		(sentence_count / paragraph_count.to_f).round(2)
 	end
 
 end
 
-puts "specify file:"
-# Use File method readlines to store text lines in array
-current = TextFile.new(gets.chomp)
-
-# Store number of lines in variable
-# line_count = lines.size
-
-# join text lines array into a string
-# text = lines.join
-# characters = text
-# non_white_chars = text.scan(/\S/)
-# words = text.split
-# sentences = text.split(/[\.\?\!]/)
-# paragraphs = text.split(/\n\n/)
+current = TextFile.new("text.txt")
 
 puts "line count: #{current.line_count}"
 puts "all characters: #{current.char_count}"
 puts "characters excluding spaces: #{current.real_char_count}"
-# puts "words: #{words.length}"
-# puts "sentences: #{sentences.length}"
-# puts "paragraphs: #{paragraphs.length}"
+puts "word count: #{current.word_count}"
+puts "sentences: #{current.sentence_count}"
+puts "paragraphs: #{current.paragraph_count}"
 
-# puts "average words per sentence: #{words.length / sentences.length}"
-# puts "average sentences per paragraph: #{sentences.length / paragraphs.length}"
+puts "average words per sentence: #{current.avg_words}"
+puts "average sentences per paragraph: #{current.avg_sentences}"
